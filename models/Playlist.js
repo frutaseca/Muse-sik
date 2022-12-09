1f7e4dd6f235370e939d332ccee136fc64ab13fe
Brandon <brandonrodriguezvega@gmail.com>
2022-12-08 19:10:15 -0500

    adding code to get others up to date

---
+++ models/Playlist.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Playlist extends Model {}
+
+Playlist.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      song_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'song',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'playlist',
+    }
+  );
+  
+  module.exports = Playlist;
+  
---
+++ seeds/seeds.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { User, Song, Playlist } = require('../models');
+
+const userData = require('./userData.json');
+const songData = require('./musicData.json');
+const playlistData = require('./playlistData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const song of songData) {
+    await Song.create({
+      ...song,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const playlist of playlistData) {
+    await Playlist.create({
+      ...playlist,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
